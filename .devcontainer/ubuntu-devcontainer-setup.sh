1f942a7d1600a6ad21bc51ba6f8919a51b978a07
Devan Lai <devan.lai@gmail.com>
2020-12-05 09:46:35 -0800

    Simplify dev container setup to remove building

--- .devcontainer/ubuntu-devcontainer-setup.sh
+++ .devcontainer/ubuntu-devcontainer-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Helper script to setup a new development container for PulseView
+# This installs all necessary dependencies to build PulseView and other
+# Sigrok libraries needed by PulseView.
+# It also clones the sigrok-util repo for its build script
+
 # Install Debian/Ubuntu/Mint dependencies for:
 #   libserialport
 #   libsigrok
@@ -15,10 +20,6 @@ sudo apt-get install -y autoconf autoconf-archive automake check cmake \
  python-dev python-gi-dev python-numpy python-setuptools qtbase5-dev swig
 
 # Clone the sigrok-util repository for the cross-compile script
-if [ ! -d "sigrok-util" ]; then
-    git clone --depth 1  https://github.com/sigrokproject/sigrok-util.git
+if [ ! -d "../sigrok-util" ]; then
+    git clone --depth 1  https://github.com/sigrokproject/sigrok-util.git ../sigrok-util
 fi
-
-# Build all the things
-cd sigrok-util/cross-compile/linux
-./sigrok-cross-linux
