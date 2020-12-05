ac4c64443fe75d884290d0c7da8be057f31e9817
Devan Lai <devan.lai@gmail.com>
2020-12-05 08:31:04 -0800

    Add experimental GitHub Codespaces config

---
+++ .devcontainer/ubuntu-devcontainer-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Install Debian/Ubuntu/Mint dependencies for:
+#   libserialport
+#   libsigrok
+#   libsigrokdecode
+#   sigrok-cli
+#   PulseView
+sudo apt-get install -y autoconf autoconf-archive automake check cmake \
+ default-jdk doxygen g++ gcc git-core libboost-filesystem-dev \
+ libboost-serialization-dev libboost-system-dev libboost-test-dev \
+ libftdi1-dev libglib2.0-dev libglibmm-2.4-dev libqt5svg5-dev libtool \
+ libusb-1.0-0-dev libzip-dev make nettle-dev pkg-config python3-dev \
+ python-dev python-gi-dev python-numpy python-setup-tools qtbase5-dev swig
+
+# Clone the sigrok-util repository for the cross-compile script
+git clone --depth 1  https://github.com/sigrokproject/sigrok-util.git
+
+# Build all the things
+cd sigrok-util/cross-compile/linux
+./sigrok-cross-linux
