19ce367585c7e54c213ccd97a8ef5d23f9e68011
Devan Lai <devan.lai@gmail.com>
2020-12-05 08:55:13 -0800

    Fix python-setuptools dependency

--- .devcontainer/ubuntu-devcontainer-setup.sh
+++ .devcontainer/ubuntu-devcontainer-setup.sh
@@ -11,7 +11,7 @@ sudo apt-get install -y autoconf autoconf-archive automake check cmake \
  libboost-serialization-dev libboost-system-dev libboost-test-dev \
  libftdi1-dev libglib2.0-dev libglibmm-2.4-dev libqt5svg5-dev libtool \
  libusb-1.0-0-dev libzip-dev make nettle-dev pkg-config python3-dev \
- python-dev python-gi-dev python-numpy python-setup-tools qtbase5-dev swig
+ python-dev python-gi-dev python-numpy python-setuptools qtbase5-dev swig
 
 # Clone the sigrok-util repository for the cross-compile script
 git clone --depth 1  https://github.com/sigrokproject/sigrok-util.git
